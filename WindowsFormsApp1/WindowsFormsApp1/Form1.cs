d9a04e7f17f2093854efec143e8d3b66977023c7
Patrick <patrick.moura@cubotecnologia.com.br>
2019-07-30 10:46:15 -0300

    Curso_C#_AULA1

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Teste Botão 1");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string nome = "Patrick", sobrenome = "Moura", nomeCompleto;
+
+            nomeCompleto = $"Nome:{nome} Sobrenome:{sobrenome}";
+
+            MessageBox.Show(nomeCompleto);
+            
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            int numerosInteiros;
+            string tipoTexto;
+            decimal numeroDecimal;
+            double numedoDecimalMenor;
+            short numeroInteiroMenor;
+            byte numeroInteiromenor;
+            DateTime dataHora;
+            bool verdadeiroouFalso;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            new Form2().Show();
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnOk_Click(object sender, EventArgs e)
+        {
+            bool valido = true;
+            if (txtNome.Text == string.Empty)
+            {
+                MessageBox.Show("Preencer o Nome", "erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                valido = false;
+            }
+            if (txtSobrenome.Text == string.Empty)
+            {
+                MessageBox.Show("Preencer o Sobre Nome", "erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                valido = false;
+            }
+            if (txtDtNascimento.Text == string.Empty)
+            {
+                MessageBox.Show("Preencer a data de Nascimento", "erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                valido = false;
+            }
+
+            if (valido == false)
+            {
+                return;
+            }
+            
+            string nome, sobrenome;
+            DateTime dataNascimento;
+
+            nome = txtNome.Text;
+            sobrenome = txtSobrenome.Text;
+            dataNascimento = Convert.ToDateTime(txtDtNascimento.Text);
+
+
+
+            
+
+            MessageBox.Show($"Nome: {nome} Sobrenome: {sobrenome} Data de Nascimento: {dataNascimento}");
+        }
+
+    }
+}
+//
